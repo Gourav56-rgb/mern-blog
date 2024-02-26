14a009517b05d3ef135b9aa2de2a3aff223b706d
GOURAV KUMAR BEHERA <85735841+Gourav56-rgb@users.noreply.github.com>
2024-02-26 15:09:02 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,7 +13,7 @@ import OnlyAdminPrivateRoute from "./components/OnlyAdminPrivateRoute";
 import CreatePost from "./pages/CreatePost";
 import UpdatePost from "./pages/UpdatePost";
 import PostPage from "./pages/PostPage";
-import ScrollToTop from "./components/ScrollTotop";
+import ScrollToTop from "./components/ScrollToTop";
 import Search from "./pages/Search";
 
 export default function App() {
